2eeba3107711d04eb9bb297999654bad5bcf6432
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-23 18:12:37 -0400

    timeblocks uploaded

---
+++ script.js
@@ -0,0 +1,20 @@
+function init() {
+    buildTimeblocks();
+}
+
+function buildTimeblocks() {
+    var tblBody = $('#timeblocks tbody');
+    var timeIds = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+
+    for(var i = 0; i < timeIds.length; i++){
+        tblBody.append(`
+            <tr class="time-block-row">
+                <td class="hour">${timeIds[i]}</td>
+                <td class="time-block">Events</td>
+                <td class="saveBtn"><i class="fas fa-save"></i></td>
+            </tr>`
+        );
+    }
+}
+
+init();
